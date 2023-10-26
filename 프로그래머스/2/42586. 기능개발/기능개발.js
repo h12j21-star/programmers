752e806c4149fae49d76d5273a8c58ff5594d1eb
h12j21-star <74497080+h12j21-star@users.noreply.github.com>
2023-10-26 16:21:37 +0900

    [level 2] Title: 기능개발, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,26 @@
+function solution(progresses, speeds) {
+    let answer = [];
+    let obj = {};
+    let count = 0;
+    progresses.forEach((progress,index)=>{
+        let last = Math.ceil((100-progress) / speeds[index])
+        obj[index] = last
+    })
+     let max = obj[0];
+    for(let [key,value] of Object.entries(obj)){
+        if(max===value){
+             count++;
+        }
+        else if(max<value){
+            max=value
+            answer.push(count)
+            count=1;
+            
+        }else if(max>value){
+            count++;
+        }
+        
+    }
+    answer.push(count)
+    return answer;
+}
